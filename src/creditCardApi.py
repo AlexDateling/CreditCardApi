5df177fc15160021101bb4dfd0afb7b8af56e515
AlexDateling <ADateling@Gmail.com>
2022-12-01 19:32:55 +0200

    added v2 validate with dynamic endpoint

--- src/creditCardApi.py
+++ src/creditCardApi.py
@@ -13,7 +13,7 @@ def home():
     return "CreditCard Validator"
 
 
-@app.route('/api/validate/')
+@app.route('/api_v1/validate/')
 def validate_credit_card():
     payload = request.get_json("creditCard")
 
@@ -26,8 +26,19 @@ def validate_credit_card():
             print("The Credit card issued is invalid ")
             return jsonify({"isValid": False}), 200
     else:
-        return (jsonify({"error": "Credit card number must be numeric"}), 200)
+        return (jsonify({"error": "Credit card number must be numeric"}), 400)
 
+@app.route('/api_v2/validate/<str: creditCard>')
+def validate_credit_card(creditCard):
+    if creditCard.isdigit():
+        if is_valid_credit_card(creditCard):
+            print("The Credit card issued is valid ")
+            return jsonify({"isValid": True}), 200
+        else:
+            print("The Credit card issued is invalid ")
+            return jsonify({"isValid": False}), 200
+    else:
+        return (jsonify({"error": "Credit card number must be numeric"}), 400)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001, debug=True)
