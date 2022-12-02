b2b9ab4e99e57c0132d64d7cc934b832788c29d5
AlexDateling <ADateling@Gmail.com>
2022-12-02 15:23:08 +0200

    refactor(removed duplicate code)

--- src/creditCardApi.py
+++ src/creditCardApi.py
@@ -4,41 +4,34 @@ from utils import is_valid_credit_card
 
 app = Flask(__name__)
 
-# isValid = is_valid_credit_card("49927398717")
-# print(isValid)
 
+def is_credit_card_valid(creditCard):
+    if creditCard.isdigit():
+        if is_valid_credit_card(creditCard):
+            print("The Credit card issued is valid ")
+            return jsonify({"isValid": True}), 200
+        else:
+            print("The Credit card issued is invalid ")
+            return jsonify({"isValid": False}), 200
+    else:
+        return (jsonify({"error": "Credit card number must be numeric"}), 400)
 
 @app.route('/')
 def home():
     return "CreditCard Validator"
 
+@app.route('/api/validate/<string:creditCard>')
+def validate_credit_card(creditCard):
+    return is_credit_card_valid(creditCard)
+    
 
-@app.route('/api_v1/validate/')
+@app.route('/api_v2/validate/')
 def validate_credit_card_json():
     payload = request.get_json("creditCard")
-
     creditCard = payload['creditCard']
-    if creditCard.isdigit():
-        if is_valid_credit_card(creditCard):
-            print("The Credit card issued is valid ")
-            return jsonify({"isValid": True}), 200
-        else:
-            print("The Credit card issued is invalid ")
-            return jsonify({"isValid": False}), 200
-    else:
-        return (jsonify({"error": "Credit card number must be numeric"}), 400)
+    return is_credit_card_valid(creditCard)
 
-@app.route('/api_v2/validate/<string:creditCard>')
-def validate_credit_card(creditCard):
-    if creditCard.isdigit():
-        if is_valid_credit_card(creditCard):
-            print("The Credit card issued is valid ")
-            return jsonify({"isValid": True}), 200
-        else:
-            print("The Credit card issued is invalid ")
-            return jsonify({"isValid": False}), 200
-    else:
-        return (jsonify({"error": "Credit card number must be numeric"}), 400)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001, debug=True)
